3b41e93ab5a7b26335a18b0b933668a15dfac3f4
AJB <designbyamber1@gmail.com>
2022-04-07 20:18:57 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,133 @@
-const express = require('express')
-const app = express();
-const PORT = 3001;
+const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const notes = require('./db/db.json');
+// Helper method for generating unique ids
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET request for notes
+app.get('/api/notes', (req, res) => {
+  // Send a message to the client
+  res.json(`${req.method} request received to get notes`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get notes`);
+});
+
+// GET request for a single note
+app.get('/api/notes/:note_id', (req, res) => {
+  if (req.body && req.params.note_id) {
+    console.info(`${req.method} request received to get a single a note`);
+    const noteId = req.params.note_id;
+    for (let i = 0; i < notes.length; i++) {
+      const currentNote = notes[i];
+      if (currentNote.note_id === noteId) {
+        res.json(currentNote);
+        return;
+      }
+    }
+    res.json('Note ID not found');
+  }
+});
+
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { noteTitle, noteText } = req.body;
+
+  // If all the required properties are present
+  if (noteTitle && noteText) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    // Obtain existing notes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new note
+        parsedNotes.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json('Error in note');
+  }
+});
+
+// GET request for upvotes
+app.get('/api/upvotes', (req, res) => {
+  // Inform the client
+  res.json(`${req.method} request received to retrieve upvote count`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to retrieve upvote count`);
+});
+
+// Post request to upvote a note
+app.post('/api/upvotes/:note_id', (req, res) => {
+  // Log our request to the terminal
+  if (req.body && req.params.note_id && req.body.upvote) {
+    console.info(`${req.method} request received to upvote a note`);
+
+    // Log the request body
+    console.info(req.body);
+
+    const noteId = req.params.note_id;
+    const requestedUpvote = req.body.upvote;
+
+    for (let i = 0; i < notes.length; i++) {
+      const currentNote = notes[i];
+      // console.log(currentNote.note_id, noteId);
+      if (currentNote.note_id === noteId && requestedUpvote) {
+        currentNote.upvotes += 1;
+        res.json(`New upvote count is: ${currentNote.upvotes}`);
+        return;
+      }
+    }
+    res.json('Note ID not found');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
