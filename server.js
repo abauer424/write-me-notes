5b80ed344a6f376adea656f2f441263eabf129e3
AJB <designbyamber1@gmail.com>
2022-04-12 20:51:05 -0400

    u

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('./Develop/public'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
